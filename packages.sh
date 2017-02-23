fa08970c22aef45d168e76ed32fbadee518210c1
ㅤ <osxusr@users.noreply.github.com>
2017-02-23 15:00:40 -0500

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 dpkg-deb -bZgzip projects/icons debs
 dpkg-deb -bZgzip projects/i7walls debs
-dpkg-deb -b Package
+dpkg-deb -bZgzip projects/taiguntether83x debs
 dpkg-deb -b Package2
 dpkg-deb -b Package3
