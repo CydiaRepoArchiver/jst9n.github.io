d7c9de256f40fbda65dfe4723f379650c82bab11
ㅤ <osxusr@users.noreply.github.com>
2017-02-25 15:04:56 -0500

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -4,5 +4,5 @@ dpkg-deb -bZgzip projects/i7walls debs
 dpkg-deb -bZgzip projects/taiguntether83x debs
 dpkg-deb -bZgzip projects/flex debs
 dpkg-deb -bZgzip projects/xCon debs
-dpkg-deb -b Package2
+dpkg-deb -bZgzip projects/ishshit debs
 dpkg-deb -b Package3
