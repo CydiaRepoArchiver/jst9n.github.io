412860a8efe1585547840ef5b8b79bdc92e68861
2Klasic <derobi@live.com>
2017-02-24 18:49:42 -0600

    test

--- packages.sh
+++ packages.sh
@@ -2,5 +2,6 @@
 dpkg-deb -bZgzip projects/icons debs
 dpkg-deb -bZgzip projects/i7walls debs
 dpkg-deb -bZgzip projects/taiguntether83x debs
+dpkg-deb -bZgzip projects/flex debs
 dpkg-deb -b Package2
 dpkg-deb -b Package3
