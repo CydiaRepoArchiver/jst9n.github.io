ca209492d030e2894215525e9dcebb5f831999d9
2Klasic <derobi@live.com>
2017-02-24 19:29:42 -0600

    added another deb

--- packages.sh
+++ packages.sh
@@ -3,5 +3,6 @@ dpkg-deb -bZgzip projects/icons debs
 dpkg-deb -bZgzip projects/i7walls debs
 dpkg-deb -bZgzip projects/taiguntether83x debs
 dpkg-deb -bZgzip projects/flex debs
+dpkg-deb -bZgzip projects/xCon debs
 dpkg-deb -b Package2
 dpkg-deb -b Package3
