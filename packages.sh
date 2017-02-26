864a5e10fd8175fb80d87fcdea65a62a9c2c42ca
2Klasic <derobi@live.com>
2017-02-26 13:14:17 -0600

    update

--- packages.sh
+++ packages.sh
@@ -5,5 +5,5 @@ dpkg-deb -bZgzip projects/taiguntether83x debs
 dpkg-deb -bZgzip projects/flex debs
 dpkg-deb -bZgzip projects/xCon debs
 dpkg-deb -bZgzip projects/ishshit debs
-dpkg-deb -bZgzip projects/Impactor debs
+dpkg-deb -bZgzip projects/impactor debs
 
