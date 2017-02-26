33e1fc579f851a9c8c7490103fe8b86867ac6a53
2Klasic <derobi@live.com>
2017-02-26 13:08:59 -0600

    impactor

--- packages.sh
+++ packages.sh
@@ -5,4 +5,5 @@ dpkg-deb -bZgzip projects/taiguntether83x debs
 dpkg-deb -bZgzip projects/flex debs
 dpkg-deb -bZgzip projects/xCon debs
 dpkg-deb -bZgzip projects/ishshit debs
+dpkg-deb -bZgzip projects/Impactor debs
 
