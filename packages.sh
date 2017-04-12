b61e0fac780a50bf447b80a64284050df0ea1a52
2Klasic <derobi@live.com>
2017-04-12 18:07:09 -0500

    updated

--- packages.sh
+++ packages.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-dpkg-deb -bZgzip projects/icons debs
 dpkg-deb -bZgzip projects/i7walls debs
-dpkg-deb -bZgzip projects/taiguntether83x debs
-dpkg-deb -bZgzip projects/flex debs
-dpkg-deb -bZgzip projects/xCon debs
-dpkg-deb -bZgzip projects/ishshit debs
-dpkg-deb -bZgzip projects/eraser debs
 dpkg-deb -bZgzip projects/SetGen debs
+
